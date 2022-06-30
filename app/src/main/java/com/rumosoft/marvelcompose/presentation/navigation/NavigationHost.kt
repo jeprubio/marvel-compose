f0df2da3cc4274afc4ec3d69a646b5cbee0b0fd5
Jep Rubio <jeprubio@gmail.com>
2022-06-30 22:42:03 +0200

    [Update] RedirectScreen is no longer needed

--- app/src/main/java/com/rumosoft/marvelcompose/presentation/navigation/NavigationHost.kt
+++ app/src/main/java/com/rumosoft/marvelcompose/presentation/navigation/NavigationHost.kt
@@ -7,28 +7,16 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import com.rumosoft.characters.presentation.navigation.NavCharItem
 import com.rumosoft.characters.presentation.navigation.charactersGraph
 import com.rumosoft.comics.presentation.navigation.comicsGraph
-import com.rumosoft.marvelcompose.presentation.screen.RedirectScreen
-
-private const val REDIRECT = "redirect"
 
 @ExperimentalAnimationApi
 @ExperimentalComposeUiApi
 @ExperimentalFoundationApi
 @Composable
 fun NavigationHost(navController: NavHostController, modifier: Modifier = Modifier) {
-    NavHost(navController, startDestination = REDIRECT, modifier = modifier) {
-        composable(REDIRECT) {
-            RedirectScreen(
-                onLoaded = {
-                    navController.popBackStack()
-                    navController.navigate(NavCharItem.Characters.route)
-                }
-            )
-        }
+    NavHost(navController, startDestination = NavCharItem.Characters.route, modifier = modifier) {
         charactersGraph(navController)
         comicsGraph(navController)
     }

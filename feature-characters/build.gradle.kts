55fc5306276f23cb770321c4fe1731b1cf155d64
jeprubio <jeprubio@gmail.com>
2023-04-18 23:21:51 +0200

    [Update] feature-characters build.gradle in kts

---
+++ feature-characters/build.gradle.kts
@@ -0,0 +1,116 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+    id("kotlin-parcelize")
+    id("shot")
+    id("com.dicedmelon.gradle.jacoco-android")
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of("17"))
+    }
+}
+
+android {
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 33
+
+        testApplicationId = "com.rumosoft.marvelcomposetest"
+        testInstrumentationRunner = "com.karumi.shot.ShotTestRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.2"
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "META-INF/LICENSE*"
+        }
+    }
+    testOptions {
+        unitTests {
+            isReturnDefaultValues = true
+            isIncludeAndroidResources = true
+        }
+    }
+    namespace = "com.rumosoft.characters"
+}
+
+dependencies {
+    val composeBom = platform(libs.compose.bom)
+    implementation(project(":library-commons"))
+    implementation(project(":library-components"))
+    implementation(composeBom)
+
+    implementation(libs.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.ui)
+    implementation(libs.material3)
+    debugImplementation(libs.ui.tooling.preview)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.activity.compose)
+
+    implementation(libs.navigation.compose)
+    implementation(libs.androidx.navigation.runtime.ktx)
+
+    // Gson + Retrofit (to perform API calls and parse the response)
+    implementation(libs.gson)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+    implementation(libs.okio)
+    implementation(libs.okhttp)
+
+    implementation(libs.coil.compose)
+
+    implementation(libs.timber)
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+    implementation(libs.hilt.navigation.compose)
+    kapt(libs.hilt.compiler)
+
+    implementation(libs.kotlinx.serialization.json)
+
+    testImplementation(project(":library-tests"))
+    testImplementation(libs.androidx.core.testing)
+
+    androidTestImplementation(composeBom)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(libs.ui.test.junit4)
+    debugImplementation(libs.ui.test.manifest)
+    androidTestImplementation(libs.junitparams)
+}
+
+shot {
+    applicationId = "com.rumosoft.marvelcomposeshot"
+}
+
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}

3bde285cd6802523e7c19f60da1a2783e4d0c401
jeprubio <jeprubio@gmail.com>
2023-04-19 23:42:59 +0200

    [Update] library-components build.gradle in kts

---
+++ library-components/build.gradle.kts
@@ -0,0 +1,83 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("kotlin-parcelize")
+    id("shot")
+    id("com.dicedmelon.gradle.jacoco-android")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of("17"))
+    }
+}
+
+android {
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 21
+
+        testApplicationId = "com.rumosoft.marvelcomposetest"
+        testInstrumentationRunner = "com.karumi.shot.ShotTestRunner"
+        consumerProguardFiles("consumer-rules.pro")
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.2"
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "META-INF/LICENSE*"
+        }
+    }
+    namespace = "com.rumosoft.components"
+}
+
+dependencies {
+    val composeBom = platform(libs.compose.bom)
+    implementation(composeBom)
+
+    implementation(libs.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.ui)
+    implementation(libs.material3)
+    debugImplementation(libs.ui.tooling)
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.activity.compose)
+
+    implementation(libs.coil.compose)
+
+    implementation(libs.lottie.compose)
+
+    androidTestImplementation(composeBom)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(libs.ui.test.junit4)
+    debugImplementation(libs.ui.test.manifest)
+}
+
+shot {
+    applicationId = "com.rumosoft.marvelcomposeshot"
+}

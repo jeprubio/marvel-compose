eee89d89b188c9cd084a634fb0fecc9eb864c491
jeprubio <jeprubio@gmail.com>
2023-09-12 23:11:00 +0200

    [Update] add another test

--- app/src/test/java/com/rumosoft/marvelcompose/KonsistTests.kt
+++ app/src/test/java/com/rumosoft/marvelcompose/KonsistTests.kt
@@ -2,9 +2,13 @@ package com.rumosoft.marvelcompose
 
 import androidx.lifecycle.ViewModel
 import com.lemonappdev.konsist.api.Konsist
+import com.lemonappdev.konsist.api.ext.list.containingFiles
+import com.lemonappdev.konsist.api.ext.list.imports
 import com.lemonappdev.konsist.api.ext.list.withAllParentsOf
+import com.lemonappdev.konsist.api.ext.list.withNameContaining
 import com.lemonappdev.konsist.api.ext.list.withNameEndingWith
 import com.lemonappdev.konsist.api.verify.assert
+import com.lemonappdev.konsist.api.verify.assertNot
 import org.junit.jupiter.api.Test
 
 class KonsistTests {
@@ -45,5 +49,15 @@ class KonsistTests {
             .classes()
             .withNameEndingWith("RepositoryImpl")
             .assert { it.resideInPackage("..data..") }
+    }
+
+    @Test
+    fun `imports in use cases should not contain android`() {
+        Konsist.scopeFromProject()
+            .classes()
+            .withNameContaining("UseCase")
+            .containingFiles
+            .imports
+            .assertNot { it.hasNameContaining("android") }
     }
 }

2bcb6e980240d4713b7d503b04e92f8df6f4aed6
jeprubio <jeprubio@gmail.com>
2026-08-08 00:03:35 +0200

    [Fix] old url

--- feature-comics/src/main/java/com/rumosoft/comics/presentation/component/ComicResults.kt
+++ feature-comics/src/main/java/com/rumosoft/comics/presentation/component/ComicResults.kt
@@ -82,7 +82,7 @@ private fun ComicResult(
             .padding(MarvelComposeTheme.paddings.smallPadding),
     ) {
         comic.thumbnail?.let {
-            val comicUrl = "http://gateway.marvel.com/v1/public/comics/${comic.id}"
+            val comicUrl = "https://marvel.antonioleiva.com/v1/public/comics/${comic.id}"
             ComicThumbnail(title = comic.title, thumbnail = it, url = comicUrl)
         }
         ComicName(comic)
